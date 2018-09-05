f7f60837783fad9ac02e5f13ed50e40b9b9890c8
Valerio Bozzolan <gnu@itisavogadro.it>
2018-09-05 22:55:46 +0200

    mibact fixer: avoid to drop multiple MiBACT identifiers

--- 2018-09-mibact-fixer/bot.php
+++ 2018-09-mibact-fixer/bot.php
@@ -19,10 +19,8 @@ wm\Wikidata::getInstance()->login();
 
 foreach( wm\Wikidata::querySPARQL( file_get_contents( 'query.sparql' ) ) as $result )  {
 
-	$code    = $result->catv->value;
 	$item_id = $result->id  ->value;
 	$item_id = str_replace( 'http://www.wikidata.org/entity/', '', $item_id );
-	$code    = str_replace( 'DBUnico.',                        '', $code    );
 
 	// pull existing data
 	$item = wm\Wikidata::getInstance()
@@ -31,20 +29,37 @@ foreach( wm\Wikidata::querySPARQL( file_get_contents( 'query.sparql' ) ) as $res
 	// data to be pushed
 	$data = $item->cloneEmpty();
 
-	// push the MiBACT ID if not exists
-	if( ! $item->hasClaimsInProperty( MIBACT_ID ) ) {
-		$data->addClaim( new wb\StatementExternalID( MIBACT_ID, $code ) );
-	}
+	// MiBACT IDs
+	$codes = [];
 
-	// push for removal of any catalog code claims
 	foreach( $item->getClaimsInProperty( CATALOG_CODE ) as $claim ) {
-		$code = $claim->getMainsnak()->getDataValue()->getValue();
-		if( false !== strpos( $code, 'DBUnico.' ) ) {
+		$value = $claim->getMainsnak()->getDataValue()->getValue();
+		if( false !== strpos( $value, 'DBUnico.' ) ) {
+
+			// push for removal this catalog code
 			$data->addClaim( $claim->cloneForRemoval() );
+
+			// remember this MiBACT ID
+			$codes[] = str_replace( 'DBUnico.', '', $value );
 		}
 	}
 
-	// push for removal of any described at URL claims
+	// check duplicate MiBACT IDs
+	foreach( $item->getClaimsInProperty( MIBACT_ID ) as $claim ) {
+		$value = $claim->getMainsnak()->getDataValue()->getValue();
+		foreach( $codes as $i => $code ) {
+			if( $code === $value ) {
+				unset( $codes[ $i ] );
+			}
+		}
+	}
+
+	// push MiBACT IDs
+	foreach( $codes as $code ) {
+		$data->addClaim( new wb\StatementExternalID( MIBACT_ID, $code ) );
+	}
+
+	// push for removal any described at URL claims
 	foreach( $item->getClaimsInProperty( DESCRIBED_AT_URL ) as $claim ) {
 		$code = $claim->getMainsnak()->getDataValue()->getValue();
 		if( false !== strpos( $code, 'http://dati.beniculturali.it/mibact/luoghi/resource/CulturalInstituteOrSite' ) ) {
