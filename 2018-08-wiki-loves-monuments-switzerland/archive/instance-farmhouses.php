cfa6fd5b9fbd695c78365dbb4342042613e7567e
Valerio Bozzolan <gnu@itisavogadro.it>
2018-09-09 18:27:24 +0200

    Wiki Loves Monuments 2018 in Switzerland: simplify instantiator of farmhouses script

--- 2018-08-wiki-loves-monuments-switzerland/archive/instance-farmhouses.php
+++ 2018-08-wiki-loves-monuments-switzerland/archive/instance-farmhouses.php
@@ -7,6 +7,9 @@
  * @license GNU GPL v3+                        *
  ***********************************************/
 
+// example diff:
+// 	https://www.wikidata.org/w/index.php?title=Q29651574&diff=prev&oldid=743339101
+
 // load boz-mw
 require '../includes/boz-mw/autoload.php';
 require __DIR__ . '/../../config.php';
@@ -28,13 +31,13 @@ ASD;
 $results = \wm\Wikidata::querySPARQL( $QUERY );
 foreach( $results as $result ) {
 	$item = str_replace( 'http://www.wikidata.org/entity/', '', $result->item->value );
-	$data = \wm\Wikidata::getInstance()
-			->login()
-			->createDataModel( $item )
-			->addClaim( new \wb\StatementItem( 'P31', 'Q489357' ) )
-			->printChanges()
-			->editEntity( [
-				'summary.pre' => '[[c:Commons:Wiki Loves Monuments 2018 in Switzerland|Wiki Loves Monuments 2018 in Switzerland]]: ',
-				'bot' => 1,
-			] );
+	\wm\Wikidata::getInstance()
+		->login()
+		->createDataModel( $item )
+		->addClaim( new \wb\StatementItem( 'P31', 'Q489357' ) )
+		->printChanges()
+		->editEntity( [
+			'summary.pre' => '[[c:Commons:Wiki Loves Monuments 2018 in Switzerland|Wiki Loves Monuments 2018 in Switzerland]]: ',
+			'bot' => 1,
+		] );
 }
